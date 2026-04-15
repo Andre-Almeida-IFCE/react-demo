7ce5cada3e1e5e84171b9f79b39e45f1ad353fb1
Andre-Almeida-IFCE <almeida.andre08@aluno.ifce.edu.br>
2026-04-15 16:13:47 -0300

    Teste_15-04

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>O André é legal demais</h1>
+      <h1>Teste de Resenha</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
